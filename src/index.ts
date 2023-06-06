b9e8e02a758e334c1b8e92c36720b05d21737bc2
Naveed Akhtar <naveed.akhtar@ketch.com>
2023-06-07 01:18:09 +0530

    chore: update code as per review comment

--- src/index.ts
+++ src/index.ts
@@ -10,27 +10,27 @@ export type ConsentStatus = {
  */
 export default function getConsent(w: Window): ConsentStatus | undefined {
   const value = getCookie(w, '_swb_ketch_')
-  if (value) {
-    try {
-      const consentObj = JSON.parse(atob(value))
-      if (consentObj && consentObj.purposes) {
-        let consent: ConsentStatus = {}
-        const p = consentObj.purposes
-        for (const purposeCode in p) {
-          if (p[purposeCode]) {
-            const x = p[purposeCode]
-            if (typeof x === 'string') {
-              consent[purposeCode] = x === 'true'
-            } else if (x.allowed) {
-              consent[purposeCode] = x.allowed === 'true'
-            }
+  if (!value) {
+    return
+  }
+  try {
+    const consentObj = JSON.parse(atob(value))
+    if (consentObj && consentObj.purposes) {
+      let consent: ConsentStatus = {}
+      const p = consentObj.purposes
+      for (const purposeCode in p) {
+        if (p[purposeCode]) {
+          const x = p[purposeCode]
+          if (typeof x === 'string') {
+            consent[purposeCode] = x === 'true'
+          } else if (x.allowed) {
+            consent[purposeCode] = x.allowed === 'true'
           }
         }
-        return consent
       }
-    } catch (e) {
-      return
+      return consent
     }
+  } catch (e) {
+    return
   }
-  return
 }
